7ffe0c2b68cc511704de910efd03d9e95c5e5c02
Kail-Stunt <Kail-Stunt@mail.ru>
2026-01-12 16:42:59 +0700

    Home Work 2.5

--- src/Main.java
+++ src/Main.java
@@ -2,12 +2,14 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        var friend = 19;
-        System.out.println("Значение исходной переменной friend = "+friend);
-        friend=friend+2;
-        System.out.println("Значение исходной переменной friend увеличенной на 2 = "+friend);
-        friend=friend/7;
-        System.out.println("Значение переменной friend делённой на 7 = "+friend);
+        var frog = 3.5;
+        System.out.println("Значение исходной переменной frog = "+frog);
+        frog=frog*10;
+        System.out.println("Значение исходной переменной frog, увеличенной в 10 раз = "+frog);
+        frog=frog/3.5;
+        System.out.println("Значение переменной frog, делённой на 3.5 = "+frog);
+        frog=frog+4;
+        System.out.println("Значение переменной frog, увеличенной на 4 = "+frog);
 
     }
 }
