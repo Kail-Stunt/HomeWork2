4bff892b0e2d2121816856280d9251e2f8dc14a0
Kail-Stunt <Kail-Stunt@mail.ru>
2026-01-12 16:15:34 +0700

    Home Work 2.1

--- src/Main.java
+++ src/Main.java
@@ -2,9 +2,12 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        var dog = 8;
+        var cat = 3.6;
+        var paper = 763789;
+        System.out.println("Значение переменной dog = "+dog);
+        System.out.println("Значение переменной cat = "+cat);
+        System.out.println("Значение переменной paper = "+paper);
 
     }
 }
