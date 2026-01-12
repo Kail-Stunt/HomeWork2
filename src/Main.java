8f72578c32d7ac55cc077c55ccc2d95e42eae6a5
Kail-Stunt <Kail-Stunt@mail.ru>
2026-01-12 17:02:22 +0700

    Home Work 2.8

--- src/Main.java
+++ src/Main.java
@@ -2,9 +2,11 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        var boxer_1 = 78.2;
-        var boxer_2 = 82.7;
-        var massDiv = boxer_2 % boxer_1;
-        System.out.println("Отсаток от деления масс двух боксёров = " + massDiv);
+        var hours = 640;
+        var allEmpl = hours / 8;
+        System.out.println("Всего работников в компании - " + allEmpl + " человек.");
+        var moreEmpl = allEmpl + 94;
+        var newHours = hours / moreEmpl;
+        System.out.println("Если в компании работает " + moreEmpl + " человека, то всего " + newHours + " часа работы может быть поделено между сотрудниками");
         }
 }
