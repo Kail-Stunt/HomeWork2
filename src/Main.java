f7173f94931904554dbc38804672c4dab86a0b97
Kail-Stunt <Kail-Stunt@mail.ru>
2026-01-12 16:31:11 +0700

    Home Work 2.3

--- src/Main.java
+++ src/Main.java
@@ -2,18 +2,12 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        var dog = 8.0;
-        var cat = 3.6;
-        var paper = 763789;
-        dog=dog+4;
-        cat=cat+4;
-        paper=paper+4;
-        dog=dog-3.5;
-        cat=cat-1.6;
-        paper=paper-7639;
-        System.out.println("Значение переменной dog = "+dog);
-        System.out.println("Значение переменной cat = "+cat);
-        System.out.println("Значение переменной paper = "+paper);
+        var friend = 19;
+        System.out.println("Значение исходной переменной friend = "+friend);
+        friend=friend+2;
+        System.out.println("Значение исходной переменной friend = "+friend);
+        friend=friend/7;
+        System.out.println("Значение исходной переменной friend = "+friend);
 
     }
 }
