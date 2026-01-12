632675d74a72dced3b479bd56c7500c22431dfa7
Kail-Stunt <Kail-Stunt@mail.ru>
2026-01-12 16:52:24 +0700

    Home Work 2.7

--- src/Main.java
+++ src/Main.java
@@ -4,9 +4,7 @@ public class Main {
     public static void main(String[] args) {
         var boxer_1 = 78.2;
         var boxer_2 = 82.7;
-        var mass1 = boxer_1 + boxer_2;
-        System.out.println("Общая масса двух боксёров = " + mass1);
-        var mass2 = Math.abs(boxer_1 - boxer_2);
-        System.out.println("Разница между массами двух боксёров = "+mass2);
+        var massDiv = boxer_2 % boxer_1;
+        System.out.println("Отсаток от деления масс двух боксёров = " + massDiv);
         }
 }
