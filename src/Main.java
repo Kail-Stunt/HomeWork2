1a59ee49af65c521d68e438925e231c9f06e141b
Kail-Stunt <Kail-Stunt@mail.ru>
2026-01-12 16:49:32 +0700

    Home Work 2.6

--- src/Main.java
+++ src/Main.java
@@ -2,14 +2,11 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        var frog = 3.5;
-        System.out.println("Значение исходной переменной frog = "+frog);
-        frog=frog*10;
-        System.out.println("Значение исходной переменной frog, увеличенной в 10 раз = "+frog);
-        frog=frog/3.5;
-        System.out.println("Значение переменной frog, делённой на 3.5 = "+frog);
-        frog=frog+4;
-        System.out.println("Значение переменной frog, увеличенной на 4 = "+frog);
-
-    }
+        var boxer_1 = 78.2;
+        var boxer_2 = 82.7;
+        var mass1 = boxer_1 + boxer_2;
+        System.out.println("Общая масса двух боксёров = " + mass1);
+        var mass2 = Math.abs(boxer_1 - boxer_2);
+        System.out.println("Разница между массами двух боксёров = "+mass2);
+        }
 }
